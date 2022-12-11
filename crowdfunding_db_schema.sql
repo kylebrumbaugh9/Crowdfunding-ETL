d1b6d2abd8e62f871b3a3f756a358dc3df222dd6
Kyle <kyle.brumbaugh9@gmail.com>
2022-12-11 12:58:22 -0500

    Adding Deliverable 3

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -3,7 +3,7 @@
 
 
 CREATE TABLE "campaign" (
-    "cd_id" int   NOT NULL,
+    "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
     "company_name" varchar(100)   NOT NULL,
     "description" text   NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE "campaign" (
     "category_id" varchar(10)   NOT NULL,
     "subcategory_id" varchar(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cd_id"
+        "cf_id"
      )
 );
 
@@ -48,6 +48,17 @@ CREATE TABLE "contacts" (
      )
 );
 
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
 
@@ -56,4 +67,7 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
 
